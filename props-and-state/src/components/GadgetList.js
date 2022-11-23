614e9b9765b99c7ebe1bccd5e4c4d0d1804b7893
Salma Ali <salma.abdifatah30@gmail.com>
2022-11-23 20:59:18 +0000

    react session 4

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+function Gadget({item}) {
+  return (
+    <div>
+      <h3>{item.title}</h3>
+      <img src={item.src} alt={item.title} /> 
+    </div>
+  )
+} 
+
+
+
 const GadgetList = (props) => {
 
   return (
     <main>
       <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+      {props.items.map((item) => (<Gadget item={item} key={item.title}/>))}
     </main>
   );
 }
