623991060236c34c3adc880b194d3083188e7974
Salma Ali <salma.abdifatah30@gmail.com>
2022-11-23 16:46:28 +0000

    react session 1

---
+++ react-webpage/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function ClickableButton() {
+
+  const [clicked, setClick] = React.useState(false);
+
+  if (clicked) {
+    return React.createElement(
+        'h1', {
+        style: {
+            color: 'green',
+            fontWeight: 'bold'
+        },
+        
+        },
+        'Hello World!'
+    );
+  }
+
+  return React.createElement(
+    'button', {
+    style: {
+      backgroundColor: "#3392e4",
+      color: "#fff",
+      border: "0",
+      padding: "10px 20px",
+      fontWeight: "bold",
+      borderRadius: "5px",
+      fontSize: "1.1em"
+    },
+    onClick: () => setClick(true)
+  },
+    'Press Here'
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(React.createElement(ClickableButton));
